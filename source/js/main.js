879e24ddf7640a529baf74481c3218b8467195bd
Aleksei0575 <chudnovets0575@gmail.com>
2019-05-12 19:30:30 +0300

    Добавил интерактивную карту, прогнал по PerfectPixel

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,10 @@
 var navMain = document.querySelector('.main-navigation');
 var navToggle = document.querySelector('.main-navigation__button');
 
+// var order = document.querySelector('.button-js');
+// var popup = document.querySelector('.modal');
+// var close = popup.querySelector('.close');
+
 navMain.classList.remove('main-navigation--nojs');
 
 navToggle.addEventListener('click', function() {
@@ -12,3 +16,24 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-navigation--opened');
   }
 });
+
+// order.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   console.log('Click');
+//   popup.classList.toggle('modal--closed');
+// });
+
+
+// for (var i = 0; i < order.length; i++) {
+//   var activeBtn = order[i];
+
+//   activeBtn.addEventListener('click', function(evt) {
+//     evt.preventDefault();
+//     popup.classList.toggle('modal--closed');
+//   });
+// }
+
+// close.addEventListener('click', function(evt) {
+//   evt.preventDefault();
+//   popup.classList.add('modal--closed');
+// });
