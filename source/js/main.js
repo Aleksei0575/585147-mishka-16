28371370d1b8a8aa16466ff27c476f2448763e44
Aleksei0575 <chudnovets0575@gmail.com>
2019-05-14 14:28:48 +0300

    Устранил замечания наставника

--- source/js/main.js
+++ source/js/main.js
@@ -1,31 +1,30 @@
-var navMain = document.querySelector('.main-navigation');
-var navToggle = document.querySelector('.main-navigation__button');
+var navMain = document.querySelector(".main-navigation");
+var navToggle = document.querySelector(".main-navigation__button");
 
-// var order = document.querySelector('.button-js');
-// var popup = document.querySelector('.modal');
-// var close = popup.querySelector('.close');
+navMain.classList.remove("main-navigation--nojs");
 
-navMain.classList.remove('main-navigation--nojs');
-
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-navigation--closed')) {
-    navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-navigation--closed")) {
+    navMain.classList.remove("main-navigation--closed");
+    navMain.classList.add("main-navigation--opened");
   } else {
-    navMain.classList.add('main-navigation--closed');
-    navMain.classList.remove('main-navigation--opened');
+    navMain.classList.add("main-navigation--closed");
+    navMain.classList.remove("main-navigation--opened");
   }
 });
 
-// order.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   console.log('Click');
-//   popup.classList.toggle('modal--closed');
-// });
 
+var byLink = document.querySelector(".button-js");
+var popup = document.querySelector(".modal--closed");
+var byClosed = popup.querySelector(".close");
+
+byLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("modal--closed");
+});
 
 // for (var i = 0; i < order.length; i++) {
-//   var activeBtn = order[i];
+//   var activeBtn = byLink[i];
 
 //   activeBtn.addEventListener('click', function(evt) {
 //     evt.preventDefault();
@@ -33,7 +32,7 @@ navToggle.addEventListener('click', function() {
 //   });
 // }
 
-// close.addEventListener('click', function(evt) {
+// byClosed.addEventListener('click', function(evt) {
 //   evt.preventDefault();
 //   popup.classList.add('modal--closed');
 // });
